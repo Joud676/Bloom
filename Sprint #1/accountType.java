3c5de63b2e0edf6b691c74cb04e6de7b8289069c
Hajar-h <hajar7abeeb@gmail.com>
2024-10-05 14:20:23 +0300

    first commit

---
+++ Sprint #1/accountType.java
@@ -0,0 +1,6 @@
+
+class accountType{
+    public static void main(String[] args){
+        System.out.println("1.2.  Users shall select their account type (seller or customer) during registration ");
+    }
+}
