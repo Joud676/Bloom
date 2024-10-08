0103703f54493a29351657fedfbb5ff2cd4bc062
Lama M. Noor <Lammun92@gmail.com>
2024-10-08 19:03:23 +0300

    remove class

--- Sprint #1/accountType.java
+++
@@ -1,6 +0,0 @@
-
-class accountType{
-    public static void main(String[] args){
-        System.out.println("1.2.  Users shall select their account type (seller or customer) during registration ");
-    }
-}
