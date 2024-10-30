f038b92c870bd5d05df11cb92b346cd24dd5a9ab
Ranad <rnadaljhdly88@gmail.com>
2024-10-30 18:05:57 +0300

    customer and seller page home

---
+++ Bloom/src/main/java/bloom/plantshop/SellerHomePageController.java
@@ -0,0 +1,47 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+
+public class SellerHomePageController {
+
+    @FXML
+    private Button Home_button;
+
+    @FXML
+    private Button addPlant_Button;
+
+    @FXML
+    private Button back_Button;
+
+    @FXML
+    private Button cart_button;
+
+    @FXML
+    private Pane main_par;
+
+    @FXML
+    private Button setting_button;
+
+    @FXML
+    private Button viewPlant_Button;
+
+    @FXML
+    void addPlant(ActionEvent event) {
+    	
+
+    }
+
+    @FXML
+    void onClick_button(ActionEvent event) {
+
+    }
+
+    @FXML
+    void viewPlant(ActionEvent event) {
+
+    }
+
+}
